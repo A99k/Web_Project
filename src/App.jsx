9332a9ab1a4b237f7ab370438ef225b220953b7f
Ankit-mca-25 <ankitkumar812772@gmail.com>
2025-09-16 07:16:41 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from './components/Header'
-import Main from './components/Main'
+// import Main from './components/Main'
+import About from './components/About'
+import Contact from './components/Contact'
 import './App.css'
 function App() {
   
 
   return (
     <>
-      <div >
+      <div>
         <Header />
-        <Main />
+        <Contact />
         </div>
     </>
   )
---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const About = () => {
+  return (
+    <div className="p-6 md:p-12">
+      <div className='mx-4 md:mx-0 grid grid-cols-1 md:grid-cols-12 gap-4'>
+        <div className='order-1 md:order-2 col-span-1 md:col-start-7 md:col-span-4 flex justify-center items-center h-64 md:h-screen border-b-2 md:border-b-0 md:border-l-2 border-gray-300'>
+          <img src="./assets/developer.svg" alt="developer" className='w-3/4'/>
+        </div>
+        
+        <div className='order-2 md:order-1 col-span-1 md:col-start-2 md:col-span-5 flex flex-col justify-around h-64 md:h-screen md:mb-0'>
+          <div className=''>
+            <p className='text-4xl font-extrabold'>About Me</p>
+            <p className='mt-2'>Hi, I’m Ankit, a passionate web developer who loves crafting modern, responsive, and user-friendly websites. I specialize in front-end development using HTML, CSS, JavaScript, and React, and I’m also experienced in Node.js and MongoDB for back-end solutions. My goal is to create digital experiences that are both functional and visually appealing. When I’m not coding, I explore new technologies, improve my design skills, and work on open-source projects. Check out my work and let’s build something amazing together!</p>
+          </div>
+          <a><span className='underline text-xl font-medium '>View My Projects</span></a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
